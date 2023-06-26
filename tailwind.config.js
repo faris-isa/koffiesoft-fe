e39508435c6a89c675f1a71effd5ca6ab496ea27
faris-isa <m.faris.isa@gmail.com>
2023-06-26 19:17:21 +0700

    add with tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      backgroundImage: {}
+    },
+    fontSize: {
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+  },
+  plugins: [],
+}
